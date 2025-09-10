40a3dc0c8ee2b7fd0eada58473f8ef3c782c293a
savickuse <savickuse@users.noreply.github.com>
2025-09-10 23:36:05 +0300

    refactor: CI/CD pipeline changes

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 31
 module.exports = API;
+
+
+// API module for AdventureGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 33
+module.exports = API;
