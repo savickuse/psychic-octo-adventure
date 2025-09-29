b1b66c51125b1058ef143528107f0d145a105468
savickuse <savickuse@users.noreply.github.com>
2025-09-29 08:20:58 +0300

    chore: add comments

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 16
 module.exports = API;
+
+
+// API module for AdventureGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 17
+module.exports = API;
