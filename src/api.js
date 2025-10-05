7ca29f4f35a880c287f6f68a344bc005bb1704df
savickuse <savickuse@users.noreply.github.com>
2025-10-05 16:58:37 +0300

    style: improve error handling

---
+++ src/api.js
@@ -0,0 +1,25 @@
+// API module for AdventureGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 7
+module.exports = API;
