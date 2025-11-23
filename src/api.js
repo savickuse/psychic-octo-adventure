a68b65ff54a480d964bec4a23e62430719f73931
savickuse <savickuse@users.noreply.github.com>
2025-11-23 23:53:21 +0200

    improve: update documentation

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 24
 module.exports = API;
+
+
+// API module for AdventureGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 29
+module.exports = API;
