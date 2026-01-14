ca418c2b4c1399df789fbf22f2c07c150892c5f9
savickuse <savickuse@users.noreply.github.com>
2026-01-14 11:44:25 +0200

    remove(utils): code cleanup

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 17
 module.exports = API;
+
+
+// API module for AdventureGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 24
+module.exports = API;
